2bf14d57a57c769c948ae85a8669be67b7beda40
Helder Ardachnikoff Gomes <tomate@MacBook-Pro-de-Helder.local>
2021-04-23 21:35:44 -0300

    implementacao da criacao das tabelas via script sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS cliente (
+   cliente_id uuid DEFAULT uuid_generate_v4 (),
+   nome VARCHAR NOT NULL,
+   email VARCHAR UNIQUE NOT NULL,
+   PRIMARY KEY (cliente_id)
+);
+
+CREATE TABLE IF NOT EXISTS produtos_favoritos (
+   produto_id uuid DEFAULT uuid_generate_v4 (),
+   cliente_id uuid NOT NULL,
+   image VARCHAR NOT NULL,
+   price VARCHAR UNIQUE NOT NULL,
+   title VARCHAR NOT NULL,
+   reviewScore VARCHAR UNIQUE NOT NULL,
+   PRIMARY KEY (produto_id, cliente_id),
+   FOREIGN KEY (cliente_id)
+        REFERENCES cliente (cliente_id)
+);
